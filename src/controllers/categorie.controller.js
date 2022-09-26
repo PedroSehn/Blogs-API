c8c8636b129b93b1e6a159beec3d5489eec98dc4
PedroSehn <pedrorsehn@hotmail.com>
2022-09-26 17:49:17 -0300

    add ex9

--- src/controllers/categorie.controller.js
+++ src/controllers/categorie.controller.js
@@ -2,11 +2,16 @@ const CategoryService = require('../services/caterogy.service');
 
 const createCategory = async (rec, res, _next) => {
     const { name } = rec.body;
-    console.log('===========>>', rec.body, '<<===============');
     const result = await CategoryService.createCategory(name);
     return res.status(201).json(result);
 };
 
+const getAllCategories = async (rec, res, _next) => {
+    const result = await CategoryService.getAllCategories();
+    return res.status(200).json(result);
+};
+
 module.exports = {
     createCategory,
+    getAllCategories,
 };
--- src/routers/categorie.router.js
+++ src/routers/categorie.router.js
@@ -3,10 +3,11 @@ const LoginController = require('../controllers/login.controller');
 const CategoryController = require('../controllers/categorie.controller');
 const CategoryValidator = require('../middleweres/categoryValidator');
 
-const { createCategory } = CategoryController;
+const { createCategory, getAllCategories } = CategoryController;
 const { isNameValid } = CategoryValidator;
 const { validateToken } = LoginController;
 
 caterogy.post('/', validateToken, isNameValid, createCategory);
+caterogy.get('/', validateToken, getAllCategories);
 
 module.exports = caterogy;
